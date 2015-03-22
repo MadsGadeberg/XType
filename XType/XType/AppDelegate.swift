0f992c2d3bdd10ba5276099a4d794b1e9d6dfb54
MadsGadeberg <Mads.Gadeberg@gmail.com>
2015-03-22 08:15:22 +0100

    Initial build

---
+++ XType/XType/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  XType
+//
+//  Created by Mads Gadeberg Jensen on 22/03/15.
+//  Copyright (c) 2015 Mads Gadeberg Jensen. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ XType/XType/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  XType
+//
+//  Created by Mads Gadeberg Jensen on 22/03/15.
+//  Copyright (c) 2015 Mads Gadeberg Jensen. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
